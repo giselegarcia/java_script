1738be26859018900fa9ca20c2e05b9cdf02d798
VAB\GiseleLuz <giseleluz@aguiabranca.com.br>
2023-08-12 11:32:58 -0300

    exercicio1

---
+++ curso.js
@@ -0,0 +1,6 @@
+//Curso de JavaScript e TypeScript do básico ao avançado
+let nome = "Gisele"; const currentDate = new Date();
+const currentHour = currentDate.getHours();  
+
+console.log("Meu nome é: " + nome + " e eu estou assistindo essa aula ás " + currentHour + " horas da manhã");
+
